44e0cdf1b589a84a5c83e66dc37b3fa833c1d460
Stuart Eaves <stuart.eaves@dudesolutions.com>
2017-02-15 13:26:26 -0500

    removed http call for results json, prefill results into angular constant

--- reporter.js
+++ reporter.js
@@ -5,58 +5,59 @@ const path = require('path');
 
 class Reporter {
 
-  constructor(options) {
-    this.options = options;
-    this._sequence = [];
-    this._counts = {};
+    constructor(options) {
+        this.options = options;
+        this._sequence = [];
+        this._counts = {};
 
-    if (!fs.existsSync(this.options.path)){
-      fs.mkdirSync(this.options.path);
+        if (!fs.existsSync(this.options.path)) {
+            fs.mkdirSync(this.options.path);
+        }
+
     }
 
-  }
+    nowString() {
+        return (new Date()).toISOString();
+    }
 
-  nowString() {
-    return (new Date()).toISOString();
-  }
-
-  jasmineStarted(suiteInfo) {
-    //this._sequence.push(suiteInfo);
-  };
-
-  suiteStarted(result) {
-    //this._sequence.push(result);
-  };
-
-  specStarted(result) {
-    this._start = this.nowString();
-    //this._sequence.push(result);
-  };
-
-  specDone(result) {
-    result.stoped = this.nowString();
-    result.started = this._start;
-    this._sequence.push(result);
-    this._counts[result.status] = (this._counts[result.status] || 0) + 1;
-  };
-
-  suiteDone(result) {
-    //this._sequence.push(result);
-  };
-
-  jasmineDone() {
-    let logEntry = {
-      sequence: this._sequence,
-      counts: this._counts
+    jasmineStarted(suiteInfo) {
+        //this._sequence.push(suiteInfo);
     };
 
-    let templatePath = path.join(__dirname, 'report.html');
-    let destination = path.join(this.options.path, 'report.html');
-    let fileContents = fs.readFileSync(templatePath).toString();
+    suiteStarted(result) {
+        //this._sequence.push(result);
+    };
 
-    fs.writeFileSync(destination, fileContents, 'utf8');
-    fs.writeFileSync(path.resolve(this.options.path, 'results.json'), JSON.stringify(logEntry, null, 4), 'utf8');
-  };
+    specStarted(result) {
+        this._start = this.nowString();
+        //this._sequence.push(result);
+    };
+
+    specDone(result) {
+        result.stoped = this.nowString();
+        result.started = this._start;
+        this._sequence.push(result);
+        this._counts[result.status] = (this._counts[result.status] || 0) + 1;
+    };
+
+    suiteDone(result) {
+        //this._sequence.push(result);
+    };
+
+    jasmineDone() {
+        let logEntry = {
+            sequence: this._sequence,
+            counts: this._counts
+        };
+
+        let templatePath = path.join(__dirname, 'report.html');
+        let destination = path.join(this.options.path, 'report.html');
+        let fileContents = fs.readFileSync(templatePath).toString();
+
+        fileContents = fileContents.replace('\'<Results Replacement>\'', JSON.stringify(logEntry, null, 4));
+
+        fs.writeFileSync(destination, fileContents, 'utf8');
+    };
 }
 
 module.exports = Reporter;
